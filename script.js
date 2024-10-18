ce7daca492108521a0e8b998de1427c641a65d17
mehariyaniraj <210303108016@paruluniversity.ac.in>
2024-10-18 13:25:10 +0530

    bmi calclutor

---
+++ script.js
@@ -0,0 +1,29 @@
+document.getElementById("bmiform").addEventListener("submit", function (e) {
+  e.preventDefault();
+  const gender = document.getElementById("gender").value;
+  const age = parseInt(document.getElementById("age").value);
+  const heightFeet = parseInt(document.getElementById("height-feet").value);
+  const heightInches = parseInt(document.getElementById("height-inches").value);
+  const weight = parseFloat(document.getElementById("weight").value);
+
+  if (gender && age && heightFeet && heightInches && weight) {
+    const heightInMeters = (heightFeet * 12 + heightInches) * 0.0254;
+    const bmi = weight / (heightInMeters * heightInMeters);
+    const resultElement = document.getElementById("result");
+
+    let category = "";
+    if (bmi < 18.5) {
+      category = "Under weight";
+    } else if (bmi >= 18.5 && bmi < 24.9) {
+      category = "Normal weight";
+    } else if (bmi >= 25 && bmi < 29.9) {
+      category = "Over weight";
+    } else {
+      category = "Obese";
+    }
+    let resultMessage = "Your Bmi " + bmi.toFixed(2) + "<br>";
+    resultMessage += "Category" + category;
+
+    resultElement.innerHTML = resultMessage;
+  }
+});
